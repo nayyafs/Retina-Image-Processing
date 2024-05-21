ef0d987610f9c833b05729459b3dfb70b94c8042
nayyafs <110149528+nayyafs@users.noreply.github.com>
2024-05-21 09:40:44 +0700

    Add files via upload

---
+++ Eksplorasi/red_channel.m
@@ -0,0 +1,38 @@
+myFolder = 'C:\Users\Firda Sabrina\Documents\Kuli-Ah\TUGAS KULIAH\TUGAS SEM 6\Pengolahan Citra Biomedika\TUBES\MATLAB\training';
+histogram_dir = 'C:\Users\Firda Sabrina\Documents\Kuli-Ah\TUGAS KULIAH\TUGAS SEM 6\Pengolahan Citra Biomedika\TUBES\MATLAB\training\red channel image';
+
+%check if histogram_dir exist
+if ~exist(histogram_dir, 'dir')
+    mkdir(histogram_dir);
+end
+
+if ~isfolder(myFolder)
+    errorMessage = sprintf('Error: The following folder does not exist:\n%s', myFolder);
+    uiwait(warndlg(errorMessage));
+    return;
+end
+
+filePattern = fullfile(myFolder, '*.png');
+theFiles = dir(filePattern);
+
+for k = 1:length(theFiles)
+    baseFileName = theFiles(k).name;
+    fullFileName = fullfile(myFolder, baseFileName);
+    fprintf(1, 'Now reading %s\n', fullFileName);
+
+    %RGB image
+    rgb_image = imread(fullFileName);
+
+    redChannel = rgb_image(:,:,1);
+
+    figure;
+    imshow(redChannel);
+
+    %get the image file name
+    [~, name, ~] = fileparts(fullFileName);
+    
+    %save the histogram
+    saveas(gcf, fullfile(histogram_dir, [name, '_red.png']));
+
+    close(gcf);
+end
