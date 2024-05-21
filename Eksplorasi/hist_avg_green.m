986e6f39351728ad8c8f3bef403e046caa5abea2
nayyafs <110149528+nayyafs@users.noreply.github.com>
2024-05-21 09:42:52 +0700

    Add files via upload

---
+++ Eksplorasi/hist_avg_green.m
@@ -0,0 +1,21 @@
+image_dir = 'C:\Users\Firda Sabrina\Documents\Kuli-Ah\TUGAS KULIAH\TUGAS SEM 6\Pengolahan Citra Biomedika\TUBES\MATLAB\testing';
+
+image_files = dir(fullfile(image_dir, '*.png'));
+
+sum_histogram = zeros(256, 1);
+
+for idx = 1:length(image_files)
+    fullFileName = fullfile(image_files(idx).folder, image_files(idx).name);
+
+    img = imread(fullFileName);
+
+    greenChannel = rgb_image(:,:,2);
+    [counts, ~] = imhist(greenChannel);
+    sum_histogram = sum_histogram + counts;
+end
+
+%calculate avg histogram
+average_hist = sum_histogram/length(image_files);
+figure;
+bar(average_hist);
+title("Average of Green Channel");
